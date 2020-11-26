9d016093af750faabb647c10e13c153b6d474347
abouIbrahim <cm_naimi@esi.dz>
2020-11-26 23:15:22 +0100

    Loading Module 4 assignment version.2 [Naimi]

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -69,17 +69,16 @@ WARNING!!! WARNING!!!
                // My solution of the Module 4 Assignment //
 //=====================================================================//
 
-(function (window) {
+(function () {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var name in names) {
   
-  var firstLetter = (names[name]).charAt(0);
-  firstLetter = firstLetter.toLowerCase();
+  var firstLetter = names[name].charAt(0).toLowerCase();
   
-  if (firstLetter == 'j') {
+  if (firstLetter === 'j') {
     byeSpeaker.speak(names[name]);
   } else {
     helloSpeaker.speak(names[name]);
   }
 }
-})(window); 
+})(); 
